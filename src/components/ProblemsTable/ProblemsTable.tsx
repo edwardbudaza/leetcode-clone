af7fe885c097a5d13816a2e14bdf20feb7828a5e
edwardbudaza <edward.budaza@yahoo.co.za>
2023-06-25 06:10:08 +0200

    added topbar for the home page

---
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type ProblemsTableProps = {
+    
+};
+
+const ProblemsTable:React.FC<ProblemsTableProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default ProblemsTable;
